0a4fc68b8ad3bfe6319bb4fda9ef1516751210e9
Yannik Bramkamp <y.bramkamp@mittwald.de>
2020-12-11 14:06:25 +0100

    wip

--- pkg/source/mysqlrestore/cli.go
+++ pkg/source/mysqlrestore/cli.go
@@ -12,6 +12,20 @@ type Options struct {
 }
 
 type Flags struct {
+	AutoRehash bool `flag:"--auto-rehash"`
+	AutoVerticalInput bool `flag:"--auto-vertical-output"`
+	Batch bool `flag:"--batch"`
+	BinaryAsHex bool `flag:"--binary-as-hex"`
+	BinaryMode bool `flag:"--binary-mode"`
+	BindAddress string `flag:"--bind-address="`
+	CharacterSetsDir string `flag:"--character-sets-dir="`
+	ColumnNames bool `flag:"--column-names"`
+	ColumnTypeInfo bool `flag:"column-type-info"`
+	Comments bool `flag:"--comments"`
+	Compress bool `flag:"--compress"`
+	CompressionAlgorithms string `flag:"--compression-algorithms="`
+	ConnectExpiredPassword bool `flag:"--connect-expired-password"`
+	ConnectTimeout int `flag:"--connect-timeout"`
 	Database              string `flag:"--database" valiudate:"min=1"`
 	Debug                 string `flag:"--debug="`
 	DebugCheck            bool   `flag:"--debug-check"`
@@ -46,67 +60,54 @@ type Flags struct {
 	XML                   bool   `flag:"--xml"`
 	ZstdCompressionLevel  int    `flag:"--zstd-compression-level"`
 
-	//AutoRehash bool `flag:"--auto-rehash"`
-	//AutoVerticalInput --auto-vertical-output
-	//--batch
-	//--binary-as-hex
-	//--binary-mode
-	//--bind-address
-	//--character-sets-dir
-	//--column-names
-	//--column-type-info
-	//--comments
-	//--compress
-	//--compression-algorithms
-	//--connect-expired-password
-	//--connect-timeout
-	//--delimiter
-	//--dns-srv-name
-	//--execute
-	//--help
-	//--histignore
-	//--html
-	//--ignore-spaces
-	//--init-command
-	//--line-numbers
-	//--load-data-local-dir
-	//--local-infile
-	//--max-join-size
-	//--named-commands
-	//--net-buffer-length
-	//--network-namespace
-	//--no-auto-rehash
-	//--no-beep
-	//--no-defaults
-	//--one-database
-	//--pager
-	//--pipe
-	//--plugin-dir
-	//--print-defaults
-	//--prompt
-	//--protocol
-	//--quick
-	//--raw
-	//--reconnect
-	//--safe-updates, --i-am-a-dummy
-	//--select-limit
-	//--show-warnings
-	//--sigint-ignore
-	//--silent
-	//--skip-auto-rehash
-	//--skip-column-names
-	//--skip-line-numbers
-	//--skip-named-commands
-	//--skip-pager
-	//--skip-reconnect
-	//--ssl-mode
-	//--syslog
-	//--table
-	//--tee
-	//--unbuffered
-	//--verbose
-	//--version
-	//--vertical
-	//--wait
+
+	--delimiter
+	--dns-srv-name
+	--execute
+	--help
+	--histignore
+	--html
+	--ignore-spaces
+	--init-command
+	--line-numbers
+	--load-data-local-dir
+	--local-infile
+	--max-join-size
+	--named-commands
+	--net-buffer-length
+	--network-namespace
+	--no-auto-rehash
+	--no-beep
+	--no-defaults
+	--one-database
+	--pager
+	--pipe
+	--plugin-dir
+	--print-defaults
+	--prompt
+	--protocol
+	--quick
+	--raw
+	--reconnect
+	--safe-updates, --i-am-a-dummy
+	--select-limit
+	--show-warnings
+	--sigint-ignore
+	--silent
+	--skip-auto-rehash
+	--skip-column-names
+	--skip-line-numbers
+	--skip-named-commands
+	--skip-pager
+	--skip-reconnect
+	--ssl-mode
+	--syslog
+	--table
+	--tee
+	--unbuffered
+	--verbose
+	--version
+	--vertical
+	--wait
 
 }
