f2deffa7964d37ad635675241236c7b6c694b29e
Yannik Bramkamp <y.bramkamp@mittwald.de>
2020-12-14 15:20:07 +0100

    added redisrestore

---
+++ cmd/redisrestore.go
@@ -0,0 +1,31 @@
+package cmd
+
+import (
+	"context"
+
+	"github.com/mittwald/brudi/pkg/source"
+	"github.com/mittwald/brudi/pkg/source/redisrestore"
+
+	"github.com/spf13/cobra"
+)
+
+var (
+	redisRestoreCmd = &cobra.Command{
+		Use:   "mongorestore",
+		Short: "restores from mongodump ",
+		Long:  "Restores a given database server with given arguments",
+		Run: func(cmd *cobra.Command, args []string) {
+			ctx, cancel := context.WithCancel(context.Background())
+			defer cancel()
+
+			err := source.DoRestoreForKind(ctx, redisrestore.Kind, cleanup, useRestic, useResticForget)
+			if err != nil {
+				panic(err)
+			}
+		},
+	}
+)
+
+func init() {
+	rootCmd.AddCommand(redisRestoreCmd)
+}
---
+++ pkg/source/redisrestore/backend_config_based.go
@@ -0,0 +1,72 @@
+package redisrestore
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/pkg/errors"
+
+	"github.com/mittwald/brudi/internal"
+	"github.com/mittwald/brudi/pkg/cli"
+)
+
+type ConfigBasedBackend struct {
+	cfg *Config
+}
+
+func NewConfigBasedBackend() (*ConfigBasedBackend, error) {
+	config := &Config{
+		&Options{
+			Flags:          &Flags{},
+			AdditionalArgs: []string{},
+		},
+	}
+
+	err := config.InitFromViper()
+	if err != nil {
+		return nil, err
+	}
+
+	return &ConfigBasedBackend{cfg: config}, nil
+}
+
+func (b *ConfigBasedBackend) RestoreBackup(ctx context.Context) error {
+	cmd := cli.CommandType{
+		Binary: binary,
+		Args:   cli.StructToCLI(b.cfg.Options),
+	}
+
+	out, err := cli.Run(ctx, cmd)
+	if err != nil {
+		return errors.WithStack(fmt.Errorf("%+v - %s", err, out))
+	}
+
+	return nil
+}
+
+func (b *ConfigBasedBackend) GetBackupPath() string {
+	if b.cfg.Options.Flags.Archive != "" {
+		return b.cfg.Options.Flags.Archive
+	}
+
+	return b.cfg.Options.Flags.Out
+}
+
+func (b *ConfigBasedBackend) GetHostname() string {
+	return b.cfg.Options.Flags.Host
+}
+
+func (b *ConfigBasedBackend) CleanUp() error {
+	var fileTypes []string
+
+	if b.cfg.Options.Flags.Archive != "" {
+		return os.Remove(b.cfg.Options.Flags.Archive)
+	} else if b.cfg.Options.Flags.Gzip {
+		fileTypes = append(fileTypes, ".bson.gz", ".json.gz")
+	} else {
+		fileTypes = append(fileTypes, ".bson", ".json")
+	}
+
+	return internal.ClearDirectory(b.GetBackupPath(), fileTypes...)
+}
