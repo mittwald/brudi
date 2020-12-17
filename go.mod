f8478f65e888f90753ca4b5fe10ae64b48d47d23
Yannik Bramkamp <y.bramkamp@mittwald.de>
2020-12-17 16:36:51 +0100

    added test data to first mongodb

--- go.mod
+++ go.mod
@@ -5,9 +5,7 @@ go 1.15
 require (
 	github.com/fsnotify/fsnotify v1.4.8 // indirect
 	github.com/go-playground/validator/v10 v10.2.0
-	github.com/konsorten/go-windows-terminal-sequences v1.0.2 // indirect
 	github.com/mitchellh/go-homedir v1.1.0
-	github.com/pelletier/go-toml v1.6.0 // indirect
 	github.com/pkg/errors v0.9.1
 	github.com/sirupsen/logrus v1.4.2
 	github.com/spf13/afero v1.2.2 // indirect
@@ -18,6 +16,7 @@ require (
 	github.com/spf13/viper v1.6.2
 	github.com/stretchr/testify v1.6.1
 	github.com/testcontainers/testcontainers-go v0.9.0
+	go.mongodb.org/mongo-driver v1.4.4
 	golang.org/x/crypto v0.0.0-20200302210943-78000ba7a073
 	golang.org/x/sys v0.0.0-20200302150141-5c8b2ff67527 // indirect
 	gopkg.in/ini.v1 v1.54.0 // indirect
--- test/pkg/config/source/mongodump/mongodump_test.go
+++ test/pkg/config/source/mongodump/mongodump_test.go
@@ -7,9 +7,18 @@ import (
 	"github.com/mittwald/brudi/pkg/source"
 	"github.com/spf13/viper"
 	"github.com/testcontainers/testcontainers-go"
+	"log"
 	"strings"
 	"testing"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
+
+type TestColl struct {
+	Name string
+	Age  int
+}
 
 func TestBasicMongoDBDump(t *testing.T) {
 	ctx := context.Background()
@@ -38,7 +47,35 @@ func TestBasicMongoDBDump(t *testing.T) {
 	if err != nil {
 		t.Error(err)
 	}
+	mongoPortStr := fmt.Sprint(mongoPort.Int())
+
 	defer mongoC.Terminate(ctx)
+
+	clientOptions := options.Client().ApplyURI(fmt.Sprintf("mongodb://%s:%s", ip, mongoPortStr))
+	clientAuth := options.Client().SetAuth(options.Credential{Username: "root", Password: "mongodbroot"})
+	// Connect to MongoDB
+	client, err := mongo.Connect(context.TODO(), clientOptions, clientAuth)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Check the connection
+	err = client.Ping(context.TODO(), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fooColl := TestColl{"Foo", 10}
+	barColl := TestColl{"Bar", 13}
+	gopherColl := TestColl{"Gopher", 42}
+
+	testData := []interface{}{fooColl, barColl, gopherColl}
+
+	collection := client.Database("test").Collection("testColl")
+
+	_, err = collection.InsertMany(context.TODO(), testData)
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	var testMongoConfig = []byte(fmt.Sprintf(`
       mongodump:
@@ -64,4 +101,34 @@ func TestBasicMongoDBDump(t *testing.T) {
 	if err != nil {
 		t.Error(err)
 	}
+
+	//ctxTest := context.Background()
+	//reqTest := testcontainers.ContainerRequest{
+	//	Image:        "mongo:latest",
+	//	ExposedPorts: []string{"27017/tcp"},
+	//	Env: map[string]string{
+	//		"MONGO_INITDB_ROOT_USERNAME": "root",
+	//		"MONGO_INITDB_ROOT_PASSWORD": "mongodbroot",
+	//	},
+	//}
+	//mongoTest, err := testcontainers.GenericContainer(ctxTest, testcontainers.GenericContainerRequest{
+	//	ContainerRequest: reqTest,
+	//	Started:          true,
+	//})
+	//if err != nil {
+	//	t.Error(err)
+	//}
+	//
+	//mongoTestIP, err := mongoTest.Host(ctx)
+	//if err != nil {
+	//	t.Error(err)
+	//}
+	//
+	//mongoTestPort, err := mongoTest.MappedPort(ctx, "27017/tcp")
+	//if err != nil {
+	//	t.Error(err)
+	//}
+	//mongoTestPortStr := fmt.Sprint(mongoPort.Int())
+	//
+	//defer mongoTest.Terminate(ctx)
 }
